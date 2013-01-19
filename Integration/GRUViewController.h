fdfb2d33844e27dd29d2c309018a5ae526e3748b
Steven W. Riggins <steve@geeksrus.com>
2013-01-18 16:53:23 -0800

    Moved xcode crap

---
+++ Integration/GRUViewController.h
@@ -0,0 +1,13 @@
+//
+//  GRUViewController.h
+//  Integration
+//
+//  Created by Steven W. Riggins on 1/18/13.
+//  Copyright (c) 2013 Geeks R Us. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRUViewController : UIViewController
+
+@end
---
+++ Integration/GRUViewController.m
@@ -0,0 +1,29 @@
+//
+//  GRUViewController.m
+//  Integration
+//
+//  Created by Steven W. Riggins on 1/18/13.
+//  Copyright (c) 2013 Geeks R Us. All rights reserved.
+//
+
+#import "GRUViewController.h"
+
+@interface GRUViewController ()
+
+@end
+
+@implementation GRUViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
